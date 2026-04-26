f8d35b97721cd810764ed81e2ad35a09ebf6cef3
AchiboldThanyani <s223145882@mandela.ac.za>
2026-04-26 11:38:08 +0200

    feat: add CommentAdded, SprintStarted, SprintCompleted, AddedToProject notification types

--- backend/src/ProjectManagement.Domain/Enums/NotificationType.cs
+++ backend/src/ProjectManagement.Domain/Enums/NotificationType.cs
@@ -2,9 +2,13 @@ namespace ProjectManagement.Domain.Enums;
 
 public enum NotificationType
 {
-    TaskAssigned  = 0,
-    TaskBlocked   = 1,
-    TaskOverdue   = 2,
-    TicketReplied = 3,
-    SlaBreached   = 4,
+    TaskAssigned    = 0,
+    TaskBlocked     = 1,
+    TaskOverdue     = 2,
+    TicketReplied   = 3,
+    SlaBreached     = 4,
+    CommentAdded    = 5,
+    SprintStarted   = 6,
+    SprintCompleted = 7,
+    AddedToProject  = 8,
 }
